0a4536b568735288717d5837b7891c8819efb24a
Adrian Santos <iancris31@yahoo.com>
2018-03-09 17:18:35 -0500

    Added an authLogIn for authentication

---
+++ studentLogIn/authLogIn.php
@@ -0,0 +1,18 @@
+<?php
+  //Starts the Session
+  session_start();
+  //Get the username and password
+  $username =$_POST['username'];
+  $password =$_POST['password'];
+  //Session Username
+  $_SESSION['username'] = $username;
+  if($username == 'test' && $password == '123'){
+    header("Location: index.html");
+  }else{
+    if($username == "" && $password ==""){
+      header("Location: ../index.html?error=2");
+    }else{
+    header("Location: ../index.html?error=1");
+  }
+  }
+ ?>
